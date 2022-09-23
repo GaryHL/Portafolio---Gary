d6191f0281d47340cad4b6a08e646d024f8a3914
Gary HL <103480668+GaryHL@users.noreply.github.com>
2022-09-23 20:35:10 +0200

    limpiando codigo sin uso

--- src/components/Info_sections/Info_section.jsx
+++ src/components/Info_sections/Info_section.jsx
@@ -4,20 +4,20 @@ import Tilt  from "react-vanilla-tilt";
 
 
 function Info_section(props) {
-  
+
   return (
-    <div className="container_info_section">
-      <div className="container_image" data-aos="zoom-in-down">
+    <div className="container_info_section" >
+      <div className="container_image" >
         <Tilt className="tilt" >
           <img src={props.img} />
         </Tilt>
       </div>
-      <div className="container_details" data-aos="zoom-in-down">
+      <div className="container_details" >
         <h2>
           {props.title}
           <br /> {props.title2}
         </h2>
-        <div className="lines">
+        <div className="lines" >
           <div className="line l1"></div>
           <div className="line l2"></div>
         </div>
